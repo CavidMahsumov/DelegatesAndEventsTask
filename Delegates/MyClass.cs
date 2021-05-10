6373d376e1bab6cff9aef7ad5ac286bb13103815
Cavid <mehsumovcavid@gmail.com>
2021-05-10 17:49:38 +0400

    Add project files.

---
+++ Delegates/MyClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class MyClass
+    {
+        public void Space(string word)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (i > 0 && i < word.Length)
+                    sb.Append('_');
+                sb.Append(word[i]);
+            }
+            Console.WriteLine(sb.ToString());
+
+        }
+
+
+
+        public void Reverse(string word)
+        {
+            var sb = new StringBuilder(word);
+
+            for (int i = 0, length = word.Length / 2; i < length; i++)
+            {
+                var secondIndex = word.Length - 1 - i;
+                var tmp = sb[i];
+
+                sb[i] = sb[secondIndex];
+                sb[secondIndex] = tmp;
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Word : ");
+            string word = Console.ReadLine();
+            MyClass myClass = new MyClass();
+            Func funcDell = new Func(myClass.Reverse);
+            funcDell += myClass.Space;
+            Run run = new Run();
+            run.RunFunc(funcDell, word);
+
+
+        }
+    }
+}
